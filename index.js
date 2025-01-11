e3831e616265d701fc275ca2845da456f5797770
Aashish Nair <aashishnair8925@gmail.com>
2025-01-11 14:31:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -141,82 +141,13 @@ document.getElementById('submit-button').addEventListener('click', function(even
 
     // submit the form if there are no issues
     if (fnameError.textContent=="" &&  mInitialError.textContent=="" &&  lnameError.textContent=="" &&  emailError.textContent=="" && matriculationError.textContent==""){
-        alert("Submitted form");
+        alert("Submitted form (All validation checks passed)");
     }
 
 
 })
 
 
-
-
-
-
-
-
-
-/*
-
-document.getElementById('submit-button').addEventListener('click', function(event){
-
-    event.preventDefault();
-    
-    
-    
-    const matriculation = document.getElementById("matriculation").value;
-    const matriculationError = document.getElementById("matriculation-error");
-
-    const email = document.getElementById("email").value;
-    const emailError = document.getElementById("email-error");
-
-    let isNumeric = true;
-
-    // matriculation must be filled
-
-
-    for (let i=0; i<matriculation.length;i++){
-        if (matriculation[i]<'0'||matriculation[i]>'9'){
-            isNumeric = false;
-            break;
-        }
-    }
-    if (!isNumeric){
-        matriculationError.textContent = "Matriculation must only contain numbers.";
-        matriculationError.style.color = "red";
-        
-        // scroll to the top
-        window.scrollTo({
-            top:0,
-            behavior: 'smooth'
-        });
-    }
-    else{
-        matriculationError.textContent="";
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regex for email
-
-    if(!emailRegex.test(email)){
-        emailError.textContent = "This email address is not valid.";
-        emailError.style.color = 'red';
-    }
-    else{
-        emailError.textContent = "";
-    }
-
-
-    // submit the form if there are no issues
-    if (emailError.textContent=="" && matriculationError.textContent==""){
-        alert("Submitted form");
-    }
-
-
-
-
-
-    });
-
-*/
 // validation for matriculation
 
 
